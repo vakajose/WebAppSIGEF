5138071a1368a46e7e49e7680af948abad5b5458
zhukulento <vakajose@gmail.com>
2017-04-05 23:15:50 -0400

    Agregar archivos de proyecto.

---
+++ WebAppSIGEF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppSIGEF.Startup))]
+namespace WebAppSIGEF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
